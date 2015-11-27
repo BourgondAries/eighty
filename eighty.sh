87127f88540e79554f89859d2a1fc32c4ce2aded
BourgondAries <macocio@gmail.com>
2015-11-27 07:11:55 +0100

    Initial eighty tool

---
+++ eighty.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+	maxcharacters=80
+	if [[ $# -eq 2 ]]; then
+		maxcharacters=$2
+	fi
+	characters=0
+	locked=0
+
+	while IFS= read -r -N 1 i; do
+		if [[ $locked -eq 1 ]]; then
+			if [[ $i = $'\n' ]]; then
+				locked=0
+			fi
+			printf "%c" "$i"
+		elif [[ "$characters" -eq "0" ]]; then
+			if [[ "$i" = $'\t' ]]; then
+				locked=1
+			elif [[ $i != $'\n' ]]; then
+				((characters += 1))
+			fi
+			printf "%c" "$i"
+
+		else
+			if [[ "$i" = $'\n' ]]; then
+				characters=0
+				printf '%s' "$i"
+			else
+				((characters += 1))
+				if [[ $characters -gt $maxcharacters ]]; then
+					if [[ "$i" = ' ' ]]; then
+						printf '\n'
+						characters=0
+					else
+						printf '%s' "$i"
+					fi
+				else
+					printf '%s' "$i"
+				fi
+			fi
+		fi
+	done < $1
